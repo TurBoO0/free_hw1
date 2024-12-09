efdfe253b49cecbcfaa4dff0d113a21c8960b633
TurBoO0 <127961357+TurBoO0@users.noreply.github.com>
2024-12-09 21:18:52 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,20 @@
+# 1st program:
+print(9 ** 0.5 * 5)
+
+# 2nd program:
+if 9.99 > 9.98 and 1000 != 1001:
+    print(True)
+
+# 3rd program:
+a = 2 * 2 + 2
+b = 2 * (2 + 2)
+print(a)
+print(b)
+print(a==b)
+
+#4th program:
+string = '123.456'
+print(string[4])
+ft = (float(string) * 10)
+print(ft)
+print(int(ft))
